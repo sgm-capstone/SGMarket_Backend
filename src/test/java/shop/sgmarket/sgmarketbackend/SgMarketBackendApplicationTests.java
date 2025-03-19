a7a86c5c1ad69a9e716e0381cd9ce118c0751120
dongkyunKim <kyun010713@gmail.com>
2025-03-19 10:21:39 +0900

    chore(#1): 프로젝트 생성 및 기본 설정

---
+++ src/test/java/shop/sgmarket/sgmarketbackend/SgMarketBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.sgmarket.sgmarketbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgMarketBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
