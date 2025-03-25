1afcd61566cc771b5f34bca1f5ea4af441743b53
dongkyunKim <kyun010713@gmail.com>
2025-03-25 22:40:56 +0900

    fix(#8): 멤버 레포지토리 제네릭 오류 해결

--- src/main/java/shop/sgmarket/sgmarketbackend/member/repository/MemberRepository.java
+++ src/main/java/shop/sgmarket/sgmarketbackend/member/repository/MemberRepository.java
@@ -3,5 +3,5 @@ package shop.sgmarket.sgmarketbackend.member.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import shop.sgmarket.sgmarketbackend.member.domain.Member;
 
-public interface MemberRepository extends JpaRepository<Long, Member> {
+public interface MemberRepository extends JpaRepository<Member, Long> {
 }
