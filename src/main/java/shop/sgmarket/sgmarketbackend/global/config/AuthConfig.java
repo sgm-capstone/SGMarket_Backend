3fc8ac862ce26cbce5de28f0acc7e055f4ed4ae7
dongkyunKim <kyun010713@gmail.com>
2025-04-03 18:11:03 +0900

    refactor(#8): @RequiredArgsConstructor 사용하도록 변경

--- src/main/java/shop/sgmarket/sgmarketbackend/global/config/AuthConfig.java
+++ src/main/java/shop/sgmarket/sgmarketbackend/global/config/AuthConfig.java
@@ -2,19 +2,17 @@ package shop.sgmarket.sgmarketbackend.global.config;
 
 import java.util.Map;
 import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import shop.sgmarket.sgmarketbackend.auth.domain.OAuthProvider;
 import shop.sgmarket.sgmarketbackend.auth.application.OAuthClient;
 
 @Configuration
+@RequiredArgsConstructor
 public class AuthConfig {
 
     private final Map<String, OAuthClient> oAuthClients;
-
-    public AuthConfig(Map<String, OAuthClient> oAuthClients) {
-        this.oAuthClients = oAuthClients;
-    }
 
     @Bean
     public Map<OAuthProvider, OAuthClient> oAuthClientMap() {
