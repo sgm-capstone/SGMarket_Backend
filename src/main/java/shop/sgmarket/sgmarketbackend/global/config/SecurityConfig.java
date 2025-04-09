6a45cc63a1bb6a74f164dcb437dd6a9fe6f13e5f
dongkyunKim <kyun010713@gmail.com>
2025-04-09 10:45:21 +0900

    refactor(#13): cors 설정 제거

--- src/main/java/shop/sgmarket/sgmarketbackend/global/config/SecurityConfig.java
+++ src/main/java/shop/sgmarket/sgmarketbackend/global/config/SecurityConfig.java
@@ -1,8 +1,5 @@
 package shop.sgmarket.sgmarketbackend.global.config;
 
-import static org.springframework.http.HttpHeaders.SET_COOKIE;
-import static org.springframework.security.config.Customizer.withDefaults;
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,9 +11,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import shop.sgmarket.sgmarketbackend.global.filter.JwtAuthenticationFilter;
 import shop.sgmarket.sgmarketbackend.global.security.JwtTokenProvider;
 
@@ -64,25 +58,9 @@ public class SecurityConfig {
     private void defaultFilterChain(HttpSecurity http) throws Exception {
         http.httpBasic(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
-                .cors(withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(
                         session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-    }
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        // TODO: CORS 임시 전체 허용
-        configuration.addAllowedOrigin("*");
-        configuration.addAllowedHeader("*");
-        configuration.addAllowedMethod("*");
-        configuration.setAllowCredentials(true);
-        configuration.addExposedHeader(SET_COOKIE);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
     }
 
     @Bean
