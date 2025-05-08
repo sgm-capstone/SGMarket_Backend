544e93e38271110d6626211f65c4bc348e0c2215
dongkyunKim <kyun010713@gmail.com>
2025-05-08 19:10:01 +0900

    refactor(#32): valid 어노테이션 변경

--- src/main/java/shop/sgmarket/sgmarketbackend/member/dto/request/MemberUpdateRequest.java
+++ src/main/java/shop/sgmarket/sgmarketbackend/member/dto/request/MemberUpdateRequest.java
@@ -1,13 +1,14 @@
 package shop.sgmarket.sgmarketbackend.member.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record MemberUpdateRequest(
 
-        @NotBlank(message = "위도는 비어있을 수 없습니다.")
+        @NotNull(message = "위도는 비어있을 수 없습니다.")
         Double latitude,
 
-        @NotBlank(message = "경도는 비어있을 수 없습니다.")
+        @NotNull(message = "경도는 비어있을 수 없습니다.")
         Double longitude,
 
         @NotBlank(message = "주소는 비어있을 수 없습니다.")
