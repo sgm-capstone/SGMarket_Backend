d3d31c985e08eb8cb9af5107d311bb2ef342e1d8
dongkyunKim <kyun010713@gmail.com>
2025-05-11 05:26:41 +0900

    refactor(#38): 좋아요 기능을 AuctuionController에 통합

--- src/main/java/shop/sgmarket/sgmarketbackend/auction/api/AuctionController.java
+++ src/main/java/shop/sgmarket/sgmarketbackend/auction/api/AuctionController.java
@@ -15,10 +15,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import shop.sgmarket.sgmarketbackend.auction.application.AuctionLikeService;
 import shop.sgmarket.sgmarketbackend.auction.application.AuctionService;
 import shop.sgmarket.sgmarketbackend.auction.dto.request.AuctionRegisterRequest;
 import shop.sgmarket.sgmarketbackend.auction.dto.request.AuctionUpdateRequest;
 import shop.sgmarket.sgmarketbackend.auction.dto.response.AuctionInfoResponse;
+import shop.sgmarket.sgmarketbackend.auction.dto.response.AuctionToggleLikeResponse;
 import shop.sgmarket.sgmarketbackend.global.dto.SliceResponse;
 import shop.sgmarket.sgmarketbackend.global.response.ApiResponseTemplate;
 
@@ -28,6 +30,7 @@ import shop.sgmarket.sgmarketbackend.global.response.ApiResponseTemplate;
 public class AuctionController implements AuctionDocs {
 
     private final AuctionService auctionService;
+    private final AuctionLikeService auctionLikeService;
 
     @Override
     @PostMapping(
@@ -38,6 +41,7 @@ public class AuctionController implements AuctionDocs {
             @RequestPart MultipartFile itemImage
     ) {
         AuctionInfoResponse response = auctionService.register(request, itemImage);
+        
         return ApiResponseTemplate.created("경매 등록에 성공했습니다.", response);
     }
 
@@ -45,6 +49,7 @@ public class AuctionController implements AuctionDocs {
     @GetMapping("/{auctionId}")
     public ApiResponseTemplate<AuctionInfoResponse> getAuction(@PathVariable Long auctionId) {
         AuctionInfoResponse response = auctionService.getAuction(auctionId);
+
         return ApiResponseTemplate.ok("경매 조회에 성공했습니다.", response);
     }
 
@@ -55,6 +60,7 @@ public class AuctionController implements AuctionDocs {
             @ParameterObject Pageable pageable
     ) {
         SliceResponse<AuctionInfoResponse> response = auctionService.getOtherAuctionsBySameMember(auctionId, pageable);
+
         return ApiResponseTemplate.ok("해당 작성자의 다른 경매 목록 조회에 성공했습니다.", response);
     }
 
@@ -64,8 +70,10 @@ public class AuctionController implements AuctionDocs {
             @RequestParam(value = "category", required = false) String category,
             @ParameterObject Pageable pageable
     ) {
-        return ApiResponseTemplate.ok("주소 및 카테고리 기반 경매 목록 조회에 성공했습니다.",
-                auctionService.getAuctionsByAddressAndCategory(category, pageable));
+        SliceResponse<AuctionInfoResponse> response =
+                auctionService.getAuctionsByAddressAndCategory(category, pageable);
+
+        return ApiResponseTemplate.ok("주소 및 카테고리 기반 경매 목록 조회에 성공했습니다.", response);
     }
 
     @Override
@@ -80,6 +88,7 @@ public class AuctionController implements AuctionDocs {
             @RequestPart(value = "itemImage", required = false) MultipartFile itemImage
     ) {
         AuctionInfoResponse response = auctionService.updateAuction(auctionId, request, itemImage);
+
         return ApiResponseTemplate.ok("경매 수정에 성공했습니다.", response);
     }
 
@@ -87,6 +96,16 @@ public class AuctionController implements AuctionDocs {
     @DeleteMapping("/{auctionId}")
     public ApiResponseTemplate<Void> deleteAuction(@PathVariable Long auctionId) {
         auctionService.deleteAuction(auctionId);
+
         return ApiResponseTemplate.ok("경매 삭제에 성공했습니다.", null);
+    }
+
+    @PostMapping("/{auctionId}/like")
+    public ApiResponseTemplate<AuctionToggleLikeResponse> toggleLike(
+            @PathVariable Long auctionId
+    ) {
+        AuctionToggleLikeResponse response = auctionLikeService.toggleLike(auctionId);
+
+        return ApiResponseTemplate.ok("경매 좋아요 토글에 성공했습니다.", response);
     }
 }
