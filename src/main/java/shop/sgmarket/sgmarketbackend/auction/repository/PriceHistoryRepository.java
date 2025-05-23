d34abf722748cb53bc14bb693a77606bbd1e9613
Junyeong-An <dev.jun0321@gmail.com>
2025-05-23 13:16:16 +0900

    feat(#61) 최신 가격 조회 메서드 추가

--- src/main/java/shop/sgmarket/sgmarketbackend/auction/repository/PriceHistoryRepository.java
+++ src/main/java/shop/sgmarket/sgmarketbackend/auction/repository/PriceHistoryRepository.java
@@ -1,9 +1,12 @@
 package shop.sgmarket.sgmarketbackend.auction.repository;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import shop.sgmarket.sgmarketbackend.auction.domain.PriceHistory;
 
 public interface PriceHistoryRepository extends JpaRepository<PriceHistory, Long> {
     List<PriceHistory> findAllByItemId(Long itemId);
+    Optional<PriceHistory> findTopByItemIdOrderByRecordedAtDesc(Long itemId);
 }
