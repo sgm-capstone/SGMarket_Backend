cb611bc1ec35764e226756c3fa0ebeebac1081e5
dongkyunKim <kyun010713@gmail.com>
2025-05-27 16:55:12 +0900

    feature(#63): JPQL QueryDsl로 변경

--- src/main/java/shop/sgmarket/sgmarketbackend/auction/repository/auctionLike/AuctionLikeRepository.java
+++ src/main/java/shop/sgmarket/sgmarketbackend/auction/repository/auctionLike/AuctionLikeRepository.java
@@ -1,11 +1,7 @@
 package shop.sgmarket.sgmarketbackend.auction.repository.auctionLike;
 
-import io.lettuce.core.dynamic.annotation.Param;
 import java.util.Optional;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import shop.sgmarket.sgmarketbackend.auction.domain.Auction;
 import shop.sgmarket.sgmarketbackend.auction.domain.AuctionLike;
 import shop.sgmarket.sgmarketbackend.member.domain.Member;
@@ -13,10 +9,4 @@ import shop.sgmarket.sgmarketbackend.member.domain.Member;
 public interface AuctionLikeRepository extends JpaRepository<AuctionLike, Long>, AuctionLikeRepositoryCustom {
     Optional<AuctionLike> findByAuctionAndMember(Auction auction, Member member);
     boolean existsByAuctionAndMember(Auction auction, Member member);
-    @Query("""
-        SELECT a.auction
-        FROM AuctionLike a
-        WHERE a.member = :member
-    """)
-    Slice<Auction> findAuctionsByMember(@Param("member") Member member, Pageable pageable);
 }
--- src/main/java/shop/sgmarket/sgmarketbackend/auction/repository/auctionLike/AuctionLikeRepositoryImpl.java
+++ src/main/java/shop/sgmarket/sgmarketbackend/auction/repository/auctionLike/AuctionLikeRepositoryImpl.java
@@ -3,6 +3,10 @@ package shop.sgmarket.sgmarketbackend.auction.repository.auctionLike;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
+import shop.sgmarket.sgmarketbackend.auction.domain.Auction;
 import shop.sgmarket.sgmarketbackend.auction.domain.QAuctionLike;
 import shop.sgmarket.sgmarketbackend.member.domain.Member;
 
@@ -23,5 +27,23 @@ public class AuctionLikeRepositoryImpl implements AuctionLikeRepositoryCustom {
                     auctionLike.auction.id.in(auctionIds)
                 )
                 .fetch();
+    }
+
+    @Override
+    public Slice<Auction> findAuctionsByMember(Member member, Pageable pageable) {
+        List<Auction> content = queryFactory
+                .select(auctionLike.auction)
+                .from(auctionLike)
+                .where(auctionLike.member.eq(member))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize() + 1)
+                .fetch();
+
+        boolean hasNext = content.size() > pageable.getPageSize();
+        if (hasNext) {
+            content.removeLast();
+        }
+
+        return new SliceImpl<>(content, pageable, hasNext);
     }
 }
