e275d7791def373971ec24a60a7dd982841533a5
dongkyunKim <kyun010713@gmail.com>
2025-06-02 17:10:51 +0900

    refactor(#73): 경매 시세 조회 성능 개선

--- src/main/java/shop/sgmarket/sgmarketbackend/auction/repository/pricehistory/PriceHistoryRepositoryImpl.java
+++ src/main/java/shop/sgmarket/sgmarketbackend/auction/repository/pricehistory/PriceHistoryRepositoryImpl.java
@@ -1,12 +1,11 @@
 package shop.sgmarket.sgmarketbackend.auction.repository.pricehistory;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import shop.sgmarket.sgmarketbackend.auction.domain.QPriceHistory;
-import shop.sgmarket.sgmarketbackend.auction.domain.PriceHistory;
 import shop.sgmarket.sgmarketbackend.auction.dto.response.PriceHistoryInfoResponse;
-
-import java.util.List;
+import shop.sgmarket.sgmarketbackend.auction.dto.response.QPriceHistoryInfoResponse;
 
 @RequiredArgsConstructor
 public class PriceHistoryRepositoryImpl implements PriceHistoryRepositoryCustom {
@@ -16,14 +15,16 @@ public class PriceHistoryRepositoryImpl implements PriceHistoryRepositoryCustom 
 
     @Override
     public List<PriceHistoryInfoResponse> findPriceHistoryInfoByItemId(Long itemId) {
-        List<PriceHistory> histories = queryFactory
-                .selectFrom(priceHistory)
+        return queryFactory
+                .select(new QPriceHistoryInfoResponse(
+                        priceHistory.id,
+                        priceHistory.price,
+                        priceHistory.recordedAt
+                ))
+                .from(priceHistory)
                 .where(priceHistory.item.id.eq(itemId))
                 .orderBy(priceHistory.recordedAt.asc())
                 .fetch();
-
-        return histories.stream()
-                .map(PriceHistoryInfoResponse::from)
-                .toList();
     }
+
 }
