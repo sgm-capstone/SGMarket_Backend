321ba667ef720fa31f5041c06f7a3f67242e43c6
dongkyunKim <kyun010713@gmail.com>
2025-06-05 13:43:30 +0900

    refactor(#86): joinclolum 추가

--- src/main/java/shop/sgmarket/sgmarketbackend/notification/domain/Notification.java
+++ src/main/java/shop/sgmarket/sgmarketbackend/notification/domain/Notification.java
@@ -8,6 +8,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -26,6 +27,7 @@ public class Notification extends BaseTimeEntity {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
     private Member member;
 
     @Enumerated(EnumType.STRING)
