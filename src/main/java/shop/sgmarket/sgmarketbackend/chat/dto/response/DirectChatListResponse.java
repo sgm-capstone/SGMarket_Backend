19aba5b274ad2effe00cdc17e3fbf5a58afd574d
Junyeong-An <dev.jun0321@gmail.com>
2025-06-09 17:18:43 +0900

    refactor(#102) 지역 이름, 프로필 이미지 반환

--- src/main/java/shop/sgmarket/sgmarketbackend/chat/dto/response/DirectChatListResponse.java
+++ src/main/java/shop/sgmarket/sgmarketbackend/chat/dto/response/DirectChatListResponse.java
@@ -10,6 +10,7 @@ public record DirectChatListResponse(
         @Schema(description = "상대방 닉네임") String otherUserNickname,
         @Schema(description = "상대방 프로필 이미지 URL") String otherUserProfileImage,
         @Schema(description = "마지막 메시지") String lastMessage,
+        @Schema(description = "상대방의 지역 이름") String locationName,
         @Schema(description = "마지막 메시지 시간") LocalDateTime lastMessageTime,
         @Schema(description = "연결된 상품 ID") Long itemId
 ) {}
